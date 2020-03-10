1b32525f56e0a1402186b8d0dffd82c9dbf52990
Bernardita Gotte <bgotte@hexacta.com>
2020-03-10 11:43:08 -0300

    Comienzo parte visual de index

--- resources/views/destino/edit.blade.php
+++ resources/views/destino/edit.blade.php
@@ -5,53 +5,53 @@
     <div class="offset-md-3 col-md-6">
 		<div class="card">
 			 <div class="card-header text-center">
-					Modificar {{$cargador->nombre}}
+					Modificar {{$destino->nombre}}
 			 </div>
 			 <div class="card-body" style="padding:30px">
-					<form action="{{action('DestinoController@update', '$cargador->cuit')}}" method="POST">
+					<form action="{{action('DestinoController@update', '$destino->cuit')}}" method="POST">
 						{{ method_field('PUT') }}
 						{{ csrf_field() }}
 					<div class="form-group">
 						 <label for="title">Nombre</label>
-						 <input type="text" name="nombre" id="nombre" class="form-control" value="{{$cargador->nombre}}">
+						 <input type="text" name="nombre" id="nombre" class="form-control" value="{{$destino->nombre}}">
 					</div>
 
 					<div class="form-group">
 						<label for="year">CUIT</label>
-						<input type="text" name="cuit" id="cuit" class="form-control" value="{{$cargador->cuit}}">
+						<input type="text" name="cuit" id="cuit" class="form-control" value="{{$destino->cuit}}">
 					</div>
 
                     <!-- CONDICION IVA -->
 
 					<div class="form-group">
 						<label for="director">DGR</label>
-						<input type="text" name="dgr" id="dgr" class="form-control" value="{{$cargador->dgr}}">
+						<input type="text" name="dgr" id="dgr" class="form-control" value="{{$destino->dgr}}">
 					</div>
 
                     Modificar Domicilio: <br>
 					<div class="form-group">
 						<label for="poster">Codigo Postal</label>
-						<input type="text" name="cp" id="cp" class="form-control" value="{{$cargador->cp}}">
+						<input type="text" name="cp" id="cp" class="form-control" value="{{$destino->cp}}">
 					</div>
 
                     <div class="form-group">
 						<label for="poster">Dirección</label>
-						<input type="text" name="domicilio" id="domicilio" class="form-control" value="{{$cargador->domicilio}}">
+						<input type="text" name="domicilio" id="domicilio" class="form-control" value="{{$destino->domicilio}}">
 					</div>
 
                     <div class="form-group">
 						<label for="poster">Localidad</label>
-						<input type="text" name="localidad" id="localidad" class="form-control" value="{{$cargador->localidad}}">
+						<input type="text" name="localidad" id="localidad" class="form-control" value="{{$destino->localidad}}">
 					</div>
 
                     <div class="form-group">
 						<label for="poster">Provincia</label>
-						<input type="text" name="provincia" id="provincia" class="form-control" value="{{$cargador->provincia}}">
+						<input type="text" name="provincia" id="provincia" class="form-control" value="{{$destino->provincia}}">
 					</div>
 
                     <div class="form-group">
 						<label for="poster">País</label>
-						<input type="text" name="pais" id="pais" class="form-control" value="{{$cargador->pais}}">
+						<input type="text" name="pais" id="pais" class="form-control" value="{{$destino->pais}}">
 					</div>
 
                     <!-- MODIFICAR CONTACTO  / AGREGAR / ELIMINAR -->
@@ -60,7 +60,7 @@
                         <button type="submit">Guardar</button>
 					</div>
 				</form>
-                <a href="{{ action('DestinoController@show', '$cargador->cuit') }}"><button>Salir y no guardar</button></a>
+                <a href="{{ action('DestinoController@show', '$destino->cuit') }}"><button>Salir y no guardar</button></a>
 			 </div>
 		</div>
     </div>
--- resources/views/destino/index.blade.php
+++ resources/views/destino/index.blade.php
@@ -1,18 +1,36 @@
 @extends('layout.master')
 @section('content')
 	@parent
-		<div class="row">
-		<a href="{{ action('DestinoController@create') }}"><button>Crear nuevo</button></a>
-		@foreach( $arrayCargador as $key)
-		<div class="col-xs-6 col-sm-4 col-md-3 text-center">
-		<a href="{{ action('DestinoController@show', $key->cuit )}}">
-		<!-- <img src="" style="height:200px"/> PONER IMAGEN GENERICA -->
-		<h4 style="min-height:45px;margin:5px 0 10px 0">
-		{{$key->nombre}}
-        CUIT: {{$key->cuit}}
-		</h4>
-		</a>
-		</div>
+	<head>
+         <link rel="stylesheet" type="text/css" href="{{ asset('css/showProduct.css') }}" >
+         <link rel="stylesheet" type="text/css" href="{{ asset('css/common-buttons.css') }}" >
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+	<body>
+    <div class="container">
+		@foreach( $arrayDestino as $key)
+		<a style="text-decoration:none" title="Mostrar más info" href="{{ action('DestinoController@show', $key->cuit )}}">
+		<div class="card">
+                <div class="box">
+					<div class="img">
+						<img src="{{ URL::to('/image/silo-icon.jpg') }}">
+					</div>
+					<h2>{{$key->nombre}}</h2>
+					<p>CUIT: {{$key->cuit}}</p>
+
+					<a href="{{ action('DestinoController@destroy', '$key->cuit') }}"><button class="delete-button" title="Borrar"><i class="fa fa-close"></i></button></a>
+					<a href="{{ action('DestinoController@edit', '$key->cuit') }}"><button class="edit-button" title="Editar"><i class="fa fa-pencil"></i></button></a>
+					<br><br>
+					</a>
+                </div>
+		    </div>
+
 		@endforeach
-		</div>
+		</a>
+        </div>
+        <a href="{{ action('DestinoController@create') }}"><button class="plus-button" title="Agregar destino"><i class="fa fa-plus"></i></button></a>
+	</body>	
 @endsection
+
