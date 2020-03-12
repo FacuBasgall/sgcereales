edcdd202e0489bb336b18bc9274a91c45b355ef7
bgotte <bgotte@hexacta.com>
2020-03-12 09:37:44 -0300

    Arreglo background y otros

--- resources/views/producto/edit.blade.php
+++ resources/views/producto/edit.blade.php
@@ -6,7 +6,7 @@
 		<link rel="stylesheet" type="text/css" href="{{ asset('css/forms.css') }}">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	</head>
-	<body>
+	<body style="background-image: url(/image/campo-trigo.jpg)">
 	<div class="row" style="margin-top:40px">
  	<div class="container">
 		<div class="card">
