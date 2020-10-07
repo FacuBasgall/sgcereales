4503232b3f9b300e8399ba4409971d4cd8b5ab2c
bgotte <bgotte@hexacta.com>
2020-10-07 17:51:18 -0300

    Arreglos bug excel y brrado de cosas

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,70 +1,66 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ action('Auth\LoginController@authenticate') }}">
-                        @csrf
-                        @if ($errors->any())
-                        <p class="alert alert-danger">El nombre de usuario o contraseña son incorrectos</p>
-                        @endif
-                        <div class="form-group row">
-                            <label for="username"
-                                class="col-md-4 col-form-label text-md-right">{{ __('Nombre de usuario') }}</label>
+<body>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Login') }}</div>
 
-                            <div class="col-md-6">
-                                <input id="username" type="text"
-                                    class="form-control @error('username') is-invalid @enderror" name="username"
-                                    value="{{ old('username') }}" required autocomplete="username" autofocus>
-                            </div>
-                        </div>
+                    <div class="card-body">
+                        <form method="POST" action="{{ action('Auth\LoginController@authenticate') }}">
+                            @csrf
+                            @if ($errors->any())
+                            <p class="alert alert-danger">El nombre de usuario o contraseña son incorrectos</p>
+                            @endif
+                            <div class="form-group row">
+                                <label for="username" class="col-md-4 col-form-label text-md-right">{{ __('Nombre de usuario') }}</label>
 
-                        <div class="form-group row">
-                            <label for="password"
-                                class="col-md-4 col-form-label text-md-right">{{ __('Contraseña') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password"
-                                    class="form-control @error('password') is-invalid @enderror" name="password"
-                                    required autocomplete="current-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember"
-                                        {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
+                                <div class="col-md-6">
+                                    <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
                                 </div>
                             </div>
-                        </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
+                            <div class="form-group row">
+                                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Contraseña') }}</label>
 
-                                @if (Route::has('password.request'))
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                                @endif
+                                <div class="col-md-6">
+                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                </div>
                             </div>
-                        </div>
-                    </form>
+
+                            <div class="form-group row">
+                                <div class="col-md-6 offset-md-4">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+                                        <label class="form-check-label" for="remember">
+                                            {{ __('Remember Me') }}
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-8 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Login') }}
+                                    </button>
+
+                                    @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
+                                    @endif
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+</body>
 @endsection
--- resources/views/exports/romaneo.blade.php
+++ resources/views/exports/romaneo.blade.php
@@ -1,122 +1,122 @@
 <table>
     <thead>
         <tr>
-            <th rowspan="6" colspan="6">
+            <th rowspan="6" colspan="6" valign="middle"><b>
                 {{$entregador->nombre}}<br>
                 {{$entregador->descripcion}}<br>
                 @foreach ($entregador_domicilio as $domicilio)
-                    {{$domicilio->domicilio}}, {{$domicilio->localidad}} ({{$domicilio->provincia}} - {{$domicilio->cp}})<br>
+                {{$domicilio->domicilio}}, {{$domicilio->localidad}} ({{$domicilio->provincia}} - {{$domicilio->cp}})<br>
                 @endforeach
                 @foreach ($entregador_contacto as $contacto)
-                    | {{$contacto->contacto}} |
+                | {{$contacto->contacto}} |
                 @endforeach
-            </th>
-            <th><strong>Nro Aviso</strong></th>
+            </b></th>
+            <th><b>Nro Aviso:</b></th>
             <td>{{ $aviso->nroAviso }}</td>
-            <th><strong>Fecha</strong></th>
+            <th><b>Fecha:</b></th>
             <td>{{ $aviso_entregador->fecha }}</td>
         </tr>
         <tr>
-            <th><strong>Grano</strong></th>
+            <th style="font-weight:bold">Grano:</th>
             <td>{{$producto->nombre}} {{$aviso_producto->tipo}}</td>
-            <th><strong>Cosecha</strong></th>
+            <th><b>Cosecha:</b></th>
             <td>{{$aviso_producto->cosecha}}</td>
         </tr>
         <tr>
-            <th><strong>Titular de C.P.</strong></th>
+            <th style="font-weight:bold">Titular de C.P:</th>
             <td>{{ $titular->nombre }}</td>
-            <th><strong>Procedencia</strong></th>
+            <th><b>Procedencia:</b></th>
             <td>{{$localidad->nombre}} ({{$provincia->nombre}})</td>
         </tr>
         <tr>
-            <th><strong>Remitente Comercial</strong></th>
+            <th style="font-weight:bold">Remitente Comercial:</th>
             <td>{{ $remitente->nombre }}</td>
-            <th><strong>Corredor</strong></th>
+            <th><b>Corredor:</b></th>
             <td>{{ $corredor->nombre }}</td>
         </tr>
         <tr>
-            <th><strong>Intermediario: </strong></th>
+            <th style="font-weight:bold">Intermediario:</th>
             @if (isset($intermediario->nombre))
-                <td>{{$intermediario->nombre}}</td>
+            <td>{{$intermediario->nombre}}</td>
             @else <td>-</td>
             @endif
-            <th><strong>Entregador: </strong></th>
+            <th><b>Entregador:</b></th>
             @if (isset($aviso->entregador))
-                <td>{{$aviso->entregador}}</td>
+            <td>{{$aviso->entregador}}</td>
             @else <td>-</td>
             @endif
         </tr>
         <tr>
-            <th><strong>Destinatario</strong></th>
+            <th style="font-weight:bold">Destinatario:</th>
             <td>{{ $destinatario->nombre }}</td>
-            <th><strong>Destino o Unidad</strong></th>
+            <th><b>Destino o Unidad:</b></th>
             <td>{{$aviso->lugarDescarga}}</td>
         </tr>
         <tr></tr>
         <tr>
-            <th colspan="4"><strong>Carga</strong></th>
-            <th colspan="12"><strong>Descarga</strong></th>
+            <th colspan="4"><b>Carga:</b></th>
+            <th colspan="12"><b>Descarga:</b></th>
         </tr>
         <tr>
-            <th>Matricula</th>
-            <th>C.P.</th>
-            <th>Fecha</th>
-            <th>Kilos</th>
-            <th>Fecha</th>
-            <th>Bruto</th>
-            <th>Tara</th>
-            <th>Neto</th>
-            <th>Humedad</th>
-            <th>Merma (%)</th>
-            <th>Merma (Kg.)</th>
-            <th>Neto Final</th>
-            <th>Diferencia</th>
-            <th>P.H.</th>
-            <th>Proteínas</th>
-            <th>Calidad</th>
+            <th><b>Matricula</b></th>
+            <th><b>C.P.</b></th>
+            <th><b>Fecha</b></th>
+            <th><b>Kilos</b></th>
+            <th><b>Fecha</b></th>
+            <th><b>Bruto</b></th>
+            <th><b>Tara</b></th>
+            <th><b>Neto</b></th>
+            <th><b>Humedad</b></th>
+            <th><b>Merma (%)</b></th>
+            <th><b>Merma (Kg.)</b></th>
+            <th><b>Neto Final</b></th>
+            <th><b>Diferencia</b></th>
+            <th><b>P.H.</b></th>
+            <th><b>Proteínas</b></th>
+            <th><b>Calidad</b></th>
         </tr>
     </thead>
     <tbody>
         @php $totalDescargado = 0; $mermaTotal = 0 @endphp
         @foreach ($cargas as $carga)
-            <tr>
-                <td>{{$carga->matriculaCamion}}</td>
-                <td>{{$carga->nroCartaPorte}}</td>
-                <td>{{$carga->fecha}}</td>
-                <td>{{$carga->kilos}}</td>
-                @foreach ($descargas as $descarga)
-                    @if($descarga->idCarga == $carga->idCarga)
-                        <td>{{$descarga->fecha}}</td>
-                        <td>{{$descarga->bruto}}</td>
-                        <td>{{$descarga->tara}}</td>
-                        <td>{{$descarga->bruto - $descarga->tara}}</td>
-                        @php $totalDescargado += $descarga->bruto - $descarga->tara @endphp
-                        <td>{{$descarga->humedad}}</td>
-                        <td>{{$descarga->merma}}</td>
-                        <td>{{round(($descarga->bruto - $descarga->tara) * ($descarga->merma / 100))}}</td>
-                        @php $mermaTotal += round(($descarga->bruto - $descarga->tara) * ($descarga->merma / 100)) @endphp
-                        <td>{{round(($descarga->bruto - $descarga->tara) - (($descarga->bruto - $descarga->tara) * ($descarga->merma / 100)))}}</td>
-                        <td>{{round((($descarga->bruto - $descarga->tara) - (($descarga->bruto - $descarga->tara) * ($descarga->merma / 100))) - $carga->kilos)}}</td>
-                        <td>{{$descarga->ph}}</td>
-                        <td>{{$descarga->proteina}}</td>
-                        <td>{{$descarga->calidad}}</td>
-                    @endif
-                @endforeach
-            </tr>
+        <tr>
+            <td>{{$carga->matriculaCamion}}</td>
+            <td>{{$carga->nroCartaPorte}}</td>
+            <td>{{$carga->fecha}}</td>
+            <td>{{$carga->kilos}}</td>
+            @foreach ($descargas as $descarga)
+            @if($descarga->idCarga == $carga->idCarga)
+            <td>{{$descarga->fecha}}</td>
+            <td>{{$descarga->bruto}}</td>
+            <td>{{$descarga->tara}}</td>
+            <td>{{$descarga->bruto - $descarga->tara}}</td>
+            @php $totalDescargado += $descarga->bruto - $descarga->tara @endphp
+            <td>{{$descarga->humedad}}</td>
+            <td>{{$descarga->merma}}</td>
+            <td>{{round(($descarga->bruto - $descarga->tara) * ($descarga->merma / 100))}}</td>
+            @php $mermaTotal += round(($descarga->bruto - $descarga->tara) * ($descarga->merma / 100)) @endphp
+            <td>{{round(($descarga->bruto - $descarga->tara) - (($descarga->bruto - $descarga->tara) * ($descarga->merma / 100)))}}</td>
+            <td>{{round((($descarga->bruto - $descarga->tara) - (($descarga->bruto - $descarga->tara) * ($descarga->merma / 100))) - $carga->kilos)}}</td>
+            <td>{{$descarga->ph}}</td>
+            <td>{{$descarga->proteina}}</td>
+            <td>{{$descarga->calidad}}</td>
+            @endif
+            @endforeach
+        </tr>
         @endforeach
         <tr></tr>
         <tr>
             <th colspan="4"></th>
-            <th colspan="3"><strong>Total descargado: </strong></th>
+            <th colspan="3"><b>Total descargado: </b></th>
             <td>{{$totalDescargado}}</td>
-            <th colspan="2"><strong>Merma: </strong></th>
+            <th colspan="2"><b>Merma: </b></th>
             <td>{{$mermaTotal}}</td>
-            <th><strong>Neto final: </strong></th>
+            <th><b>Neto final: </b></th>
             <td>{{$totalDescargado - $mermaTotal}}</td>
         </tr>
         <tr></tr>
         <tr>
-            <th colspan="4"><strong>Observaciones: </strong></th>
+            <th colspan="4"><b>Observaciones: </b></th>
             <td>{{$aviso->observacion}}</td>
         </tr>
     </tbody>
