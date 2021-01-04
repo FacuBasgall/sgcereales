a9aecb961bd8130d583bb2c02cdf29b928d927fc
FacuBasgall <facubasgall@gmail.com>
2021-01-04 11:52:48 -0300

    home admin view

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,4 +2,16 @@
 @section('content')
 @parent
 
+<head>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/home-admin.css') }}">
+</head>
+<body>
+    <div class="titulo">
+        <h1>¡Bienvenido Administrador!</h1>
+    </div>
+    <div class="img-admin">
+        <img src="{{ URL::to('/image/SGC.jpg') }}">
+    </div>
+</body>
+
 @endsection
