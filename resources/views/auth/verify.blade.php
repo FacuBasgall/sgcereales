668a2a8985d09e941a493ffe4ee93124b3d92a31
FacuBasgall <facubasgall@gmail.com>
2021-03-26 18:49:54 -0300

    password mail

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -9,12 +9,13 @@
 
                 <div class="card-body">
                     @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ __('A fresh verification link has been sent to your email address.') }}
+                    </div>
                     @endif
-
-                    <a href="{{ url('/password/reset/'.$token) }}">Click Here</a>
+                    <p>Hola, se solicitó un restablecimiento de contraseña para tu cuenta, haz clic en el botón que aparece a continuación para cambiar tu contraseña.
+                    Si tu no realizaste la solicitud de cambio de contraseña, solo ignora este mensaje.</p>
+                    <a href="{{ url('/password/reset/'.$token) }}">Haga clic aquí</a>
 
                 </div>
             </div>
