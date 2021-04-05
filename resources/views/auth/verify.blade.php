fd9b01881251a5cf63986edd8afbf1954f3a9f6a
bgotte <bgotte@hexacta.com>
2021-04-05 10:53:03 -0300

    fix correo contraseña

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -3,21 +3,16 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
-
-                <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
-                        </div>
-                    @endif
-
-                    <a href="{{ url('/password/reset/'.$token) }}">Click Here</a>
-
+        <div>{{ __('Verify Your Email Address') }}</div>
+            <div>
+                @if (session('resent'))
+                <div class="alert alert-success" role="alert">
+                    {{ __('A fresh verification link has been sent to your email address.') }}
                 </div>
-            </div>
+                @endif
+                <p>Hola, se solicitó un restablecimiento de contraseña para tu cuenta, haz clic en el botón que aparece a continuación para cambiar tu contraseña.
+                Si tu no realizaste la solicitud de cambio de contraseña, solo ignora este mensaje.</p>
+                <a href="{{ url('/password/reset/'.$token) }}">Haga clic aquí</a>
         </div>
     </div>
 </div>
