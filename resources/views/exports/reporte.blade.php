01f383e8d4125b80007bbd68abba26de9029bb00
bgotte <bgotte@hexacta.com>
2023-02-01 16:32:56 -0300

    Se cambia orden del excel

--- resources/views/exports/reporte.blade.php
+++ resources/views/exports/reporte.blade.php
@@ -1,31 +1,39 @@
 <table>
     <thead>
         <tr>
-            <div style="vertical-align:middle; display:inherit;">
-                <th rowspan="6" colspan="2"><b>
-                        {{$usuario->nombre}}<br>
-                        {{$usuario->descripcion}}<br>
-                        @foreach ($domicilio as $d)
-                        {{$d->calle}}, {{$d->nombreLocalidad}} ({{$d->provinciaAbrev}} -
-                        {{$d->cp}})<br>
-                        @endforeach
-                        @foreach ($contactos as $contacto)
-                        | {{$contacto->contacto}} |
-                        @endforeach
+            <th rowspan="1" colspan="3"><b>Resumen General de Avisos de Descargas</b></th>
+        </tr>
+        <div>
+            <tr>
+                <th colspan="3">
+                    <b>
+                        {{$usuario->nombre}}
                     </b>
                 </th>
-            </div>
-            <th><b>Resumen General de Avisos de Descargas</b></th>
-        </tr>
-        <tr>
-            <th><b style="font-weight:bold">Fecha desde: {{date("d/m/Y", strtotime($fechadesde))}}</b></th>
-        </tr>
-        <tr>
-            <th><b style="font-weight:bold">Fecha hasta: {{date("d/m/Y", strtotime($fechahasta))}}</b></th>
-        </tr>
-        <tr></tr>
-        <tr></tr>
-        <tr></tr>
+                <th><b style="font-weight:bold">Fecha desde: {{date("d/m/Y", strtotime($fechadesde))}}</b></th>
+            </tr>
+             <tr>
+                <th colspan="3">
+                    <b>
+                        {{$usuario->descripcion}}
+                    </b>
+                </th>
+                <th><b style="font-weight:bold">Fecha hasta: {{date("d/m/Y", strtotime($fechahasta))}}</b></th>
+            </tr>
+            <tr>
+                <th colspan="3">
+                    <b>
+                        @foreach ($domicilio as $d)
+                        {{$d->calle}}, {{$d->nombreLocalidad}} ({{$d->provinciaAbrev}} -
+                        {{$d->cp}})
+                        @endforeach
+                        @foreach ($contactos as $contacto)
+                        | {{$contacto->contacto}} | 
+                        @endforeach
+                    </b>
+                <th>
+            </tr>
+        </div>
         <tr></tr>
         <tr>
             <th><strong>Número</strong></th>
@@ -86,9 +94,9 @@
         <tr>
         </tr>
         <tr>
-            <th style="font-weight:bold">Total descargado (Kg): {{$total}}</th>
-            <th style="font-weight:bold">Total descargado con merma (Kg): {{$totalMerma}}</th>
+            <th colspan="3" style="font-weight:bold">Total descargado (Kg): {{$total}}</th>
+            <th colspan="3" style="font-weight:bold">Total descargado con merma (Kg): {{$totalMerma}}</th>
         </tr>
-        <tr></tr>
+        <tr></tr>    
     </tbody>
 </table>
